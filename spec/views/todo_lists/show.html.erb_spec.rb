a3fe2fec1ecafa706c11bd4899836052c2f05c90
hin101 <hineshpatel0@googlemail.com>
2016-08-10 19:28:48 +0100

    remove description from todo_list table

--- spec/views/todo_lists/show.html.erb_spec.rb
+++ spec/views/todo_lists/show.html.erb_spec.rb
@@ -4,7 +4,6 @@ describe "todo_lists/show" do
   before(:each) do
     @todo_list = assign(:todo_list, stub_model(TodoList,
       :title => "Title",
-      :description => "MyText"
     ))
   end
 
@@ -12,6 +11,5 @@ describe "todo_lists/show" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     rendered.should match(/Title/)
-    rendered.should match(/MyText/)
   end
 end
