8caff97382259d7074dba3715f8fe595bca917f9
Hinesh Patel <hineshpatel0@googlemail.com>
2016-08-18 14:01:09 +0100

    Add foundation form builder

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Odot
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W( #{config.root}/lib )
+
+    config.action_view.field_error_proc = Proc.new{ |html, instance| html }
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
