59ae12c188b40fc5f69c3d4a811af9769c7572b9
Hinesh Patel <hineshpatel0@googlemail.com>
2016-08-19 14:24:17 +0100

    Test user registration for tutorial

--- spec/features/users/registration_spec.rb
+++ spec/features/users/registration_spec.rb
@@ -17,5 +17,25 @@ describe "Signing up" do
 
     expect(User.count).to eq(1)
   end
+
+  it "displays a tutorial when the user signs up" do
+    expect(User.count).to eq(0)
+
+    visit "/"
+    expect(page).to have_content("Sign Up")
+    within("header") {click_link "Sign Up"}
+
+    fill_in "First Name", with: "Jason"
+    fill_in "Last Name", with: "Seifer"
+    fill_in "Email", with: "jason@teamtreehouse.com"
+    fill_in "Password", with: "treehouse1234"
+    fill_in "Password (again)", with: "treehouse1234"
+    click_button "Sign Up"
+
+    expect(page).to have_content("ODOT Tutorial")
+    click_link "ODOT Tutorial"
+
+    expect(page.all("li.todo-item").size).to eq(7)
+  end
   
 end
